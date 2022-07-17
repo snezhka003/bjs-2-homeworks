2aa61fa523764f15d3a0af4bfb4989c049b6c676
snezhka003 <snezhanka19@mail.ru>
2022-07-17 12:45:42 +0300

    Задача № 1 мелкие правки

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
-
-  return resultArr; // array
+  return arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10); // array
 }
