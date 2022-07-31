68d76fe14460e9227e5744fbe906cc1d5d7396fd
snezhka003 <snezhanka19@mail.ru>
2022-07-31 11:57:16 +0300

    Создание класса готово

---
+++ 7.async/task.js
@@ -0,0 +1,64 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+
+    addClock(time, callback, id) {
+        if (id === undefined) {
+            throw new Error ("Идентификатор будильника не передан!");
+        } else if (this.alarmCollection.some(item => item.id === id)) {
+            console.error("Будильник с этим id уже существует!");
+            callback();
+        } else {
+            this.alarmCollection.push({time, callback, id});
+        }
+    };
+
+    removeClock(id) {
+        let deleteClock = this.alarmCollection.findIndex(item => item.id === id);
+
+        if(deleteClock === -1) {
+            return false;
+        } else {
+            this.alarmCollection.splice([deleteClock], 1)[0];
+            return true;
+        }
+    };
+
+    getCurrentFormattedTime() {
+        return new Date().toTimeString().slice(0,5);
+    };
+
+    start() {
+        let checkClock = clock => {
+            if (clock.alarmCollection.time === getCurrentFormattedTime()) {
+                callback();
+            }
+        };
+
+        if (this.timerId === null) {
+            this.timerId = setInterval(() => {
+                this.alarmCollection.forEach (item => checkClock(item));
+            }, 60000);
+        }
+    };
+
+    stop() {
+        if (this.timerId !== null && this.timerId !== undefined) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    };
+
+    printAlarms() {
+        console.log(`Печать всех будильников: ${this.alarmCollection.length}`);
+
+        this.alarmCollection.forEach (item => console.log(`Будильник № ${item.id} заведен на ${item.time}`));
+    };
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection.length = 0;
+    };
+}
