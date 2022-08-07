62ecf0979747702dfa38a94f8065de9c5a7428ca
snezhka003 <snezhanka19@mail.ru>
2022-08-07 15:32:25 +0300

    ДЗ к лекции № 8 - правки

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -5,57 +5,55 @@ function cachingDecoratorNew(func) {
     const hash = args.join(',');
     let objectInCache = cache.find(item => item.hash === hash);
 
-    if (!objectInCache) {
-      let result = func(...args);
-      cache.push({hash, value: result});
-
-      if (cache.length > 5) { 
-        cache.shift();
-      }
-
-      console.log("Вычисляем: " + result);
-      return "Вычисляем: " + result;
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value; 
     }
 
-    console.log("Из кэша: " + objectInCache.value);
-    return "Из кэша: " + objectInCache.value;  
-}
+    let result = func(...args);
+    cache.push({hash, value: result});
+
+    if (cache.length > 5) { 
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
 return wrapper;
 }
 
 
 function debounceDecoratorNew(func, delay) {
-  let isTrottled = false;
+  let timeoutId = null;
 
-  return function(...args) {
-    if(!isTrottled) {
-      const result = func(...args);
-      isTrottled = true;
-  
-      setTimeout(() => isTrottled = false, delay);
-      return result;
+  return function wrapper(...args) {
+    if(timeoutId) {
+      clearTimeout(timeoutId);
     }
+
+    timeoutId = setTimeout(() => func(...args), delay);
+    setTimeout(() => timeoutId = null, delay);
   }
 }
 
 function debounceDecorator2(func, delay) {
-  let isTrottled = false;
+  let timeoutId = null;
 
   function wrapper(...args) {
-    wrapper.count.push(args);
+    wrapper.count++;
 
-    if(!isTrottled) {
-      const result = func(...args);
-      isTrottled = true;
-
-      setTimeout(() => isTrottled = false, delay);
-      return result;
+    if(timeoutId) {
+      clearTimeout(timeoutId);
     }
+
+    timeoutId = setTimeout(() => func(...args), delay);
+    setTimeout(() => timeoutId = null, delay);
   }
-  wrapper.count = [];
+  wrapper.count = 0;
 
   return wrapper;
 }
 
 // запустить в консоли
-// console.log(upgradedSendSignalToo.count.length);
+// console.log(upgradedSendSignalToo.count);
